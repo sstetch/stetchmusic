ed7e02e8340ae0237b3c0d7ee021adf5457885c9
sstetch <130715050+sstetch@users.noreply.github.com>
2023-05-13 19:47:41 +0300

    Update config.py
    Signed-off-by: sstetch <130715050+sstetch@users.noreply.github.com>

--- config.py
+++ config.py
@@ -5,21 +5,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "14837825"))
+API_HASH = getenv("API_HASH", "0ed849f5e7ab2df61d969317de2ca64c")
 
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("OWNER_ID", "1404114574")
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TXNX5")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/its_stetch")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1404114574").split()))
 
